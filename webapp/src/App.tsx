bbfc6f432f01813add15eb830327dc98731ac2e7
Pat Barber <patbarber72@gmail.com>
2023-03-17 09:11:33 +1100

    display form components, text input style

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import { Form } from './components/Form'
+import { useState } from "react";
+import "./App.css";
+import { Form } from "./components/Form";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="m-auto container">
-     <Form></Form>
+      <div className="m-4">
+        <Form></Form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- webapp/src/components/form/TextInput.tsx
+++ webapp/src/components/form/TextInput.tsx
@@ -1,7 +1,14 @@
 export const TextInput = () => {
   return (
     <>
-      <input type="text" />
+      <label htmlFor="textInput" className="text-xl">Text Input</label>
+      <input
+        type="text"
+        className="border-2 
+        border-gray-300 border-solid rounded-md p-2 
+        bg-slate-100 shadow-md 
+        focus:bg-slate-50  selection:border-solid selection:border-gray-400"
+      />
     </>
   );
 };
